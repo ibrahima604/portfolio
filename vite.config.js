fc01350151c7c731ff27f69c8aefdb9ccabd8d54
ibrahima604 <ibd8905@gmail.com>
2026-01-17 22:50:26 +0100

    Add postcss and vercel config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/MonPortfolio/',  // ← IMPORTANT pour GitHub Pages
   plugins: [
     react(),
     tailwindcss()
